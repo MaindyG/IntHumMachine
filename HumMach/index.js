bd107ff366491abc93fd043017f6b65881c18e83
ChadiEC <120136365+ChadiEC@users.noreply.github.com>
2024-02-14 11:12:00 -0500

    Add files via upload

--- HumMach/index.js
+++ HumMach/index.js
@@ -1,5 +1,3 @@
-let session;
-let media;
 let currentSession;
 let currentMediaSession;
 let isPlaying = true;
@@ -15,7 +13,119 @@ const videoList = [
     'https://transfertco.ca/video/DBillPrelude.mp4',
     'https://transfertco.ca/video/DBillSpotted.mp4',
     'https://transfertco.ca/video/usa23_7_02.mp4'
+    // Add more video URLs as needed
 ];
+
+document.getElementById('connectButton').addEventListener('click', () => {
+    initializeApiOnly();
+});
+
+document.getElementById('startBtn').addEventListener('click', () => {
+    if (currentSession) {
+        loadMedia(videoList[currentVideoIndex]);
+    } else {
+        alert('Connectez-vous sur chromecast en premier');
+    }
+});
+
+document.getElementById('nextBtn').addEventListener('click', () => {
+    if (currentSession) {
+        currentVideoIndex = (currentVideoIndex + 1) % videoList.length;
+        loadMedia(videoList[currentVideoIndex]);
+    } else {
+        alert('Connectez-vous sur chromecast en premier');
+    }
+});
+
+document.getElementById('playBtn').addEventListener('click', () => {
+    if (currentMediaSession) {
+        if (isPlaying) {
+            currentMediaSession.pause(null, onMediaCommandSuccess, onError);
+        } else {
+            currentMediaSession.play(null, onMediaCommandSuccess, onError);
+        }
+        isPlaying = !isPlaying;
+    }
+});
+
+
+function sessionListener(newSession) {
+    currentSession = newSession;
+    document.getElementById('startBtn').style.display = 'block';
+    document.getElementById('nextBtn').style.display = 'block';
+}
+
+
+
+function initializeSeekSlider(remotePlayerController, mediaSession) {
+    currentMediaSession = mediaSession;
+    document.getElementById('playBtn').style.display = 'block';
+   // Set max value of seek slider to media duration in seconds
+   seekSlider.max = mediaSession.media.duration;
+
+    // Update seek slider and time elements on time update
+    updateInterval = setInterval(() => {
+        const currentTime = mediaSession.getEstimatedTime();
+        const totalTime = mediaSession.media.duration;
+  
+        seekSlider.value = currentTime;
+        currentTimeElement.textContent = formatTime(currentTime);
+        totalTimeElement.textContent = formatTime(totalTime);
+      }, 1000); //chaque 1000 ms... 1 sec
+  
+      // slider change
+      seekSlider.addEventListener('input', () => {
+        const seekTime = parseFloat(seekSlider.value);
+        remotePlayerController.seek(seekTime);
+      });
+ }
+
+function receiverListener(availability) {
+    if (availability === chrome.cast.ReceiverAvailability.AVAILABLE) {
+        document.getElementById('connectButton').style.display = 'block';
+    } else {
+        document.getElementById('connectButton').style.display = 'none';
+    }
+}
+
+function onInitSuccess() {
+    console.log('Chromecast init success');
+}
+
+function onError(error) {
+    console.error('Chromecast initialization error', error);
+}
+
+function onMediaCommandSuccess() {
+    console.log('Media command success');
+}
+
+function initializeApiOnly() {
+    
+    const sessionRequest = new chrome.cast.SessionRequest(applicationID);
+    const apiConfig = new chrome.cast.ApiConfig(sessionRequest, sessionListener, receiverListener);
+
+    chrome.cast.initialize(apiConfig, onInitSuccess, onError);
+}
+
+function loadMedia(videoUrl) {
+    currentVideoUrl = videoUrl;
+    const mediaInfo = new chrome.cast.media.MediaInfo(videoUrl, defaultContentType);
+    const request = new chrome.cast.media.LoadRequest(mediaInfo);
+    const remotePlayer = new cast.framework.RemotePlayer();
+    const remotePlayerController = new cast.framework.RemotePlayerController(remotePlayer);
+
+    currentSession.loadMedia(request, mediaSession => {
+        console.log('Media chargé avec succès');
+        initializeSeekSlider(remotePlayerController, mediaSession);
+      }, onError);
+}
+
+function formatTime(timeInSeconds) {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = Math.floor(timeInSeconds % 60);
+    return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+}
 
 function Login(){
     document.getElementById('connectButton').addEventListener('click', () => {
@@ -57,30 +167,6 @@ function Next(){
         }
     });
 }
-
-function initializeSeekSlider(remotePlayerController, mediaSession) {
-    currentMediaSession = mediaSession;
-    document.getElementById('playBtn').style.display = 'block';
-   // Set max value of seek slider to media duration in seconds
-   seekSlider.max = mediaSession.media.duration;
-
-    // Update seek slider and time elements on time update
-    updateInterval = setInterval(() => {
-        const currentTime = mediaSession.getEstimatedTime();
-        const totalTime = mediaSession.media.duration;
-  
-        seekSlider.value = currentTime;
-        currentTimeElement.textContent = formatTime(currentTime);
-        totalTimeElement.textContent = formatTime(totalTime);
-      }, 1000); //chaque 1000 ms... 1 sec
-  
-      // slider change
-      seekSlider.addEventListener('input', () => {
-        const seekTime = parseFloat(seekSlider.value);
-        remotePlayerController.seek(seekTime);
-      });
- }  
-
 
 
 function VolumeUp() {
@@ -147,110 +233,5 @@ function Info(){
     
     });
 }
-function sessionListener(newSession) {
-    session = newSession;
-    document.getElementById('startBtn').style.display = 'block';
-    document.getElementById('nextBtn').style.display = 'block';
-}
 
 
-
-function onMediaDiscovered(mediaItem) {
-    media = mediaItem;
-    document.getElementById('playBtn').style.display = 'block';
-}
-
-function receiverListener(availability) {
-    if (availability === chrome.cast.ReceiverAvailability.AVAILABLE) {
-        document.getElementById('connectButton').style.display = 'block';
-    } else {
-        document.getElementById('connectButton').style.display = 'none';
-    }
-}
-
-function onInitSuccess() {
-    console.log('Chromecast init success');
-}
-
-function onError(error) {
-    console.error('Chromecast initialization error', error);
-}
-
-function onMediaCommandSuccess() {
-    console.log('Media command success');
-}
-
-
-//
-
-function initializeApiOnly() {
-    
-    const sessionRequest = new chrome.cast.SessionRequest(applicationID);
-    const apiConfig = new chrome.cast.ApiConfig(sessionRequest, sessionListener, receiverListener);
-
-    chrome.cast.initialize(apiConfig, onInitSuccess, onError);
-}
-
-function loadMedia(videoUrl) {
-    const mediaInfo = new chrome.cast.media.MediaInfo(videoUrl, 'video/mp4');
-    const request = new chrome.cast.media.LoadRequest(mediaInfo);
-
-    session.loadMedia(request, onMediaDiscovered, onError);
-}
-
-
-// Function to initialize the Cast SDK
-function initializeCastApi() {
-
-    // Set up Cast SDK options
-    const castOptions = new cast.framework.CastOptions();
-    castOptions.receiverApplicationId = applicationID;
-
-    // Initialize CastContext with the CastOptions
-    const castContext = cast.framework.CastContext.getInstance();
-    castContext.setOptions(castOptions);
-    
-    // Your existing event listener and button click handling code
-    const castButton = document.getElementById('castButton');
-    cast.framework.CastContext.getInstance().addEventListener(
-        cast.framework.CastContextEventType.CAST_STATE_CHANGED,
-        function(event) {
-            switch (event.castState) {
-                case cast.framework.CastState.NO_DEVICES_AVAILABLE:
-                    castButton.disabled = true;
-                    break;
-                case cast.framework.CastState.NOT_CONNECTED:
-                    castButton.disabled = false;
-                    break;
-                case cast.framework.CastState.CONNECTING:
-                case cast.framework.CastState.CONNECTED:
-                    castButton.disabled = true;
-                    break;
-            }
-        }
-    );
-
-    // Add a click event listener to the Cast button
-    castButton.addEventListener('click', function() {
-        // Get the current Cast session
-        const session = castContext.getCurrentSession();
-
-        // Check if there is an active Cast session
-        if (session) {
-            // Already connected - do nothing or disconnect if needed
-        } else {
-            // Not connected - initiate a Cast session
-            castContext.requestSession().then(
-                function() {
-                    // Handle successful connection
-                    console.log('Connected to Chromecast');
-                    initializeApiOnly();
-                },
-                function(errorCode) {
-                    // Handle connection error
-                    console.error('Error connecting to Chromecast: ' + errorCode);
-                }
-            );
-        }
-    })
-}
